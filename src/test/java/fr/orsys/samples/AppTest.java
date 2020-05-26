d149c5daceebb310ece5d0492376d46f3fcce7d9
Youssef JOUDAR <y.joudar@hotmail.com>
2020-05-26 11:51:49 +0200

    Update AppTest.java

--- src/test/java/fr/orsys/samples/AppTest.java
+++ src/test/java/fr/orsys/samples/AppTest.java
@@ -3,7 +3,6 @@ package fr.orsys.samples;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import fr.orsys.samples.App;
 
 /**
  * Unit test for simple App.
@@ -43,13 +42,6 @@ public class AppTest
     public void testApp1()
     {
         assertTrue( true);
-    }
-    
-    @Test
-    public void isGreaterTest() {
-        System.out.println("Test JUNIT");
-        App helloWorld = new App();
-        assertTrue("Num 1 is greater than Num 2", helloWorld.isGreater(4, 3));
     }
  
 }
